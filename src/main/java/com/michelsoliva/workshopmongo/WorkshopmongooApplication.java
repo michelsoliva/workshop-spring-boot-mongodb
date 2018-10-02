12cedca91b3910f0856edfd543031cf5a2eaa928
Michel Soliva <michelsolivapqd@gmail.com>
2018-10-02 16:05:50 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/michelsoliva/workshopmongo/WorkshopmongooApplication.java
@@ -0,0 +1,12 @@
+package com.michelsoliva.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongooApplication.class, args);
+	}
+}
